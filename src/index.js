69ddbb42eeb8a748a38007a95b4b15177c37dc9e
bipoza <bipoza@gmail.com>
2022-01-04 18:23:53 +0100

    Added Matomo setUserId instruction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import reducers from './reducers';
+import CustomMatomoAppExtra from './matomo/customMatomoAppExtra';
 const applyConfig = (config) => {
   config.addonReducers = {
     ...config.addonReducers,
     ...reducers,
   };
+  config.settings.appExtras = [
+    ...(config.settings.appExtras || []),
+    {
+      match: '',
+      component: CustomMatomoAppExtra,
+    },
+  ];
   return config;
 };
 
