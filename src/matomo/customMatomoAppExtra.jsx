1a7cb7fec9bb1e8f540c9cd17be1c31db29449be
bipoza <bipoza@gmail.com>
2023-06-20 16:35:54 +0200

    matomo app extra configuration

--- src/matomo/customMatomoAppExtra.jsx
+++ src/matomo/customMatomoAppExtra.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { pushInstruction } from '@eeacms/volto-matomo/utils';
+import { pushInstruction, trackSiteSearch } from '@eeacms/volto-matomo/utils';
 
 export const CustomMatomoAppExtra = () => {
   const { id } = useSelector((state) => state.users.user);
+  const { search } = useSelector((state) => state.router.location);
+
   React.useEffect(() => {
     if (id) {
       pushInstruction('setUserId', id);
     }
   }, [id]);
+
+  useEffect(() => {
+    if (search.includes('?SearchableText=')) {
+      const value = search.replace('?SearchableText=', '');
+      trackSiteSearch(value);
+    }
+  }, [search]);
 
   return <React.Fragment />;
 };
