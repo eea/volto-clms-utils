7180623cf81457af39a0063b9b983e23cb120260
ionlizarazu <ilizarazu@codesyntax.com>
2023-07-13 16:45:15 +0200

    add loading loaded functionality to the get_cart and set_cart functions to allow some more control

--- src/actions/cart/cart.js
+++ src/actions/cart/cart.js
@@ -2,6 +2,7 @@
  * Cart items actions.
  * @module actions/getCartItems
  */
+import { CART_SESSION_KEY } from '../../cart/useCartState';
 export const GET_CART_ITEMS = 'GET_CART_ITEMS';
 export const SET_CART_ITEMS = 'SET_CART_ITEMS';
 /**
@@ -9,12 +10,24 @@ export const SET_CART_ITEMS = 'SET_CART_ITEMS';
  * @function setCartItems
  * @returns {Object} Set extra items action.
  */
-export function setCartItems(items) {
-  return {
-    type: SET_CART_ITEMS,
-    items: items,
+export const setCartItems = (items) => {
+  return async (dispatch) => {
+    dispatch({
+      type: `${SET_CART_ITEMS}_PENDING`,
+    });
+    try {
+      dispatch({
+        type: `${SET_CART_ITEMS}_SUCCESS`,
+        items: items,
+      });
+    } catch (error) {
+      dispatch({
+        type: `${SET_CART_ITEMS}_ERROR`,
+        error: error,
+      });
+    }
   };
-}
+};
 
 /**
  * Get Cart items.
@@ -22,9 +35,26 @@ export function setCartItems(items) {
  * @returns {Object} Get extra items action.
  */
 
-export function getCartItems(user_id) {
-  return {
-    type: GET_CART_ITEMS,
-    user_id: user_id,
+export const getCartItems = (user_id) => {
+  return async (dispatch) => {
+    dispatch({
+      type: `${GET_CART_ITEMS}_PENDING`,
+    });
+    try {
+      const CART_SESSION_USER_KEY = user_id
+        ? CART_SESSION_KEY.concat(`_${user_id}`)
+        : CART_SESSION_KEY;
+      const items =
+        (await JSON.parse(localStorage.getItem(CART_SESSION_USER_KEY))) || [];
+      dispatch({
+        type: `${GET_CART_ITEMS}_SUCCESS`,
+        items: items,
+      });
+    } catch (error) {
+      dispatch({
+        type: `${GET_CART_ITEMS}_ERROR`,
+        error: error,
+      });
+    }
   };
-}
+};
