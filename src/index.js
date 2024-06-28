f79e5035ab9f3a6df7e667682da8e5d1b654703b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-06-28 19:08:33 +0300

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -11,9 +11,6 @@ const applyConfig = (config) => {
   // some complain from prettier
   // eslint-disable-next-line
   config.settings.loadables = {
-    'react-json-schema-form-builder': loadable.lib(() =>
-      import('@ginkgo-bioworks/react-json-schema-form-builder/dist/index'),
-    ),
     fontAwesome: loadable.lib(() => import('@fortawesome/react-fontawesome')),
     fontAwesomeLibrary: loadable.lib(() =>
       import('@fortawesome/fontawesome-svg-core'),
